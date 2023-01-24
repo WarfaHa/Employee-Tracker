3f3b45ad57e11dcf4a5cf6a5aebd081a84f963e3
Warfa <warfahassan35@yahoo.com>
2023-01-23 21:51:40 -0500

    formatted tables

--- db/schema.sql
+++ db/schema.sql
@@ -14,8 +14,7 @@ CREATE TABLE role (
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
-  ON DELETE SET NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
   PRIMARY KEY (id)
 );
 
@@ -24,6 +23,6 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
   PRIMARY KEY (id)
 );
